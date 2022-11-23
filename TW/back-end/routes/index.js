b0e808964bd0a72215c2d5e4fd08a371a1e5b772
Catina Ada <114821541+catinaada@users.noreply.github.com>
2022-11-23 17:20:29 +0200

    Delete TW directory

--- TW/back-end/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userRouter = require("./user");
-const experienceRouter = require("./experience");
-
-router.use("/users", userRouter);
-router.use("/experiences", experienceRouter);
-
-module.exports = router;
