7cede09ea5cac5e9e8a8088d9679ff2837e61f14
Ana Carp <98180213+AnaCarp@users.noreply.github.com>
2023-01-06 18:09:48 +0200

    Delete front-end/frontend directory

--- front-end/frontend/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-import * as experienceActions from './experience-actions'
-import * as userActions from './user-actions'
-
-export {
-    experienceActions, userActions
-}
--- front-end/frontend/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import experience from './experience-reducer'
-
-export default combineReducers({
-    experience
-})
